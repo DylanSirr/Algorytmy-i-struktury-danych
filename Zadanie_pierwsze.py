67432da22589fa31d74230e386ea1b0a8dad42bf
Dylan Serrarens <121444972+DylanSirr@users.noreply.github.com>
2025-03-16 19:14:41 +0100

    insert and shell

--- Zadanie_pierwsze.py
+++ Zadanie_pierwsze.py
@@ -9,4 +9,25 @@ def insertion_sort(arr):
     return arr
 
 def shell_sort(arr):
+    propper_array = arr[:]
+    k = 0
+    shell_gaps = []
+    shell_gap = 1
+    while shell_gap < len(arr):
+        shell_gaps.append(shell_gap)
+        shell_gap = 4**(k+1) + 3*2**k + 1
+        k += 1
+    shell_gaps.reverse()
     
+    for gap in shell_gaps:
+        temp_array = []
+        indices = list(range(0, len(arr), gap))
+        for i in indices:
+            temp_array.append(propper_array[i])
+        temp_array = insertion_sort(temp_array)
+        for i in indices:
+            propper_array[i] = temp_array.pop(0)
+    
+    return propper_array
+
+print(shell_sort([4,14,7,2,1,10,3,8,11,5,12]))
